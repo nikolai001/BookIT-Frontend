af19be6eb7819bb797f5256d41c9b212fc059504
nikolairaahauge <nira36696@edu.ucl.dk>
2026-01-27 21:07:56 +0100

    Initial commit

---
+++ src/Views/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const userIsPresent = ref<boolean>(true)
+
+onMounted(() => {
+  setTimeout(() => {
+    userIsPresent.value = false
+  }, 5000)
+})
+</script>
+
+<template>
+  <h1>Are you awake?</h1>
+  {{ userIsPresent }}
+</template>
